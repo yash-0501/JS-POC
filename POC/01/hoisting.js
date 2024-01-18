cb429a54cfb44b60e330c57b3a6f2ac6bdc0f4d6
Yash Agarwal <37997384+yash-0501@users.noreply.github.com>
2024-01-18 13:07:25 +0000

    hoisting

--- POC/01/hoisting.js
+++ POC/01/hoisting.js
@@ -1,8 +1,82 @@
-var x = 7
+//  ------- Normal Working ---------
 
-function getName(){
+// var x = 7
+
+// function getName(){
+//     console.log("JS POC");
+// }
+
+// getName();
+// console.log(x);
+
+// Output: 
+// JS POC
+// 7
+
+// ---------- Normal Working -------
+
+// -------- Hoisting -----------
+
+// getName();
+// console.log(x);
+
+// var x = 7
+
+// function getName(){
+//     console.log("JS POC");
+// }
+
+// Output: 
+// JS POC
+// undefined
+
+
+// ----------------------
+
+
+// getName();
+// console.log(x);
+
+
+
+// function getName(){
+//     console.log("JS POC");
+// }
+
+// Output: 
+// JS POC
+// Error x is not defined
+
+// ----------------------------
+
+// function getName(){
+//     console.log("JS POC");
+// }
+
+// console.log(getName)
+
+// Prints the function
+
+// ----------------------------
+
+// console.log(getName)
+
+// function getName(){
+//     console.log("JS POC");
+// }
+
+// Still Prints the function (Not like the variable x)
+
+// ----- In case of Arrow Functions ------
+console.log(getName);
+
+var getName = () =>{
     console.log("JS POC");
 }
 
-getName();
-console.log(x);
+//Output: undefined
+
+// The getName behaves as a variable
+
+// --------- Hoisting -------
+
